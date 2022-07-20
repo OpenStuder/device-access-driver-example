0286ff8cfe8fe8cd6837701a186ddf19abbc5205
Michael Clausen <michael.clausen@hevs.ch>
2022-07-20 14:14:53 +0200

    Added documentation to device access driver example

--- device-access/example.cpp
+++ device-access/example.cpp
@@ -2,17 +2,32 @@
 #include <sidevice.h>
 #include <sideviceaccess.h>
 
+/*
+ * Minimal example device. It offers only one property that represents a simple switch that can be turned on/off.
+ */
 class ExampleDevice: public SIDevice {
   public:
     inline explicit ExampleDevice(const QString& id): SIDevice("Example Device", id, false, SIDeviceFunction::All) {}
 
   private:
+    /**
+     * @brief This method of a device should return the list of properties that is supported by the device.
+     *
+     * @return List of properties.
+     */
     const QVector<SIProperty>& properties_() const override {
         static QVector<SIProperty> properties = {
             {42, SIPropertyType::Bool, SIAccessLevel::Basic, SIPropertyFlag::Readable | SIPropertyFlag::Writeable, "Switch", ""},
         };
         return properties;
     }
+
+    /**
+     * @brief This method should try to read a property from the device and return the value and status of the operation.
+     *
+     * @param id    ID of the property to read.
+     * @return      Result of the read operation.
+     */
     SIPropertyReadResult readProperty_(SIPropertyID id) const override {
         if (id == 42) {
             return {id, SIStatus::Success, switch_};
@@ -20,6 +35,16 @@ class ExampleDevice: public SIDevice {
             return {id, SIStatus::NoProperty, {}};
         }
     }
+
+    /**
+     * @brief This method should try to write  property to the actual device and return the status of the operation.
+     *
+     * @param id        ID of the property to write.
+     * @param value     Value to write.
+     * @param flags     Write flags. The only flag that exists is the Permanent flag. In that case the value should be written permanently so that after
+     *                  a restart of the device, the value is kept.
+     * @return          Result of the write operation.
+     */
     SIPropertyWriteResult writeProperty_(SIPropertyID id, const QVariant& value, SIPropertyWriteFlags flags) override {
         if (id == 42) {
             switch_ = value.toBool();
@@ -32,11 +57,21 @@ class ExampleDevice: public SIDevice {
     bool switch_ = false;
 };
 
+/*
+ * Minimal example device access. A device access instance should offer the functionality to enumerate devices on a bus or any other communication facility.
+ */
 class ExampleDeviceAccess: public SIDeviceAccess {
   public:
     inline explicit ExampleDeviceAccess(const QString& id): SIDeviceAccess(id) {}
 
   private:
+    /**
+     * @brief The device access should enumerate the available devices on the bus or other the communication facility and update the passed list of devices.
+     *
+     * @param devices   List of devices that are already enumerated. The device access instance has to remove the devices that are not available anymore and add
+     *                  newly detected devices to that list.
+     * @return          True if the enumeration was successful, false otherwise.
+     */
     bool enumerateDevices_(QVector<SIDevice*>& devices) override {
         if (devices.isEmpty()) {
             devices.append(new ExampleDevice("example"));
@@ -45,7 +80,7 @@ class ExampleDeviceAccess: public SIDeviceAccess {
     }
 };
 
-SIDeviceAccess* ExampleDriver::createDeviceAccessInstance(const QString& id, const QVariantMap& parameters) {
+SIDeviceAccess* ExampleDeviceAccessDriver::createDeviceAccessInstance(const QString& id, const QVariantMap& parameters) {
     Q_UNUSED(parameters)
 
     return new ExampleDeviceAccess(id);
--- device-access/example.h
+++ device-access/example.h
@@ -2,11 +2,20 @@
 #include <QObject>
 #include <sideviceaccessdriver.h>
 
-class ExampleDriver: public QObject, public SIDeviceAccessDriver {
+/*
+ * Plugin/Driver entrypoint class. Needs to implement the SIDeviceAccessDriver interface.
+ */
+class ExampleDeviceAccessDriver: public QObject, public SIDeviceAccessDriver {
+    // Q_OBJECT macro is required in order to be able to load the plugin/driver.
 	Q_OBJECT
+
+    // Refers to the plugin metadata in JSON format.
 	Q_PLUGIN_METADATA(IID SIDeviceAccessDriverPlugin_IID FILE "example.json")
+
+    // Declares that the plugin implements the SIDeviceAccessDriver interface.
 	Q_INTERFACES(SIDeviceAccessDriver)
 
   public:
+    // This method actually creates a new instance of the actual storage class.
 	SIDeviceAccess* createDeviceAccessInstance(const QString& id, const QVariantMap& parameters) override;
 };
